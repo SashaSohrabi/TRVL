42064c50ffe1136d7f5fcfa45255e0228342bb74
Sasha Sohrabi <sasha@nxte.nl>
2020-10-09 18:20:23 +0300

    addes styles for CardItem and Cards, used props in CatrdItem component

--- src/components/card-item/CardItem.jsx
+++ src/components/card-item/CardItem.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './CardItem.css';
 
-function CardItem() {
+function CardItem({ src, text, label, path }) {
   return (
     <>
       <li className="card-item cards__card-item">
-        <Link className="card-item__link">
-          <figure className="card-item__pic-wrap">
-            <img src="/" alt="Travel Image" className="card-item__img" />
+        <Link className="card-item__link" to={path}>
+          <figure className="card-item__pic-wrap" data-category={label}>
+            <img src={src} alt="Travel pic" className="card-item__img" />
           </figure>
           <div className="card-item__info">
-            <h5 className="card-item__text" />
+            <h5 className="card-item__text">{text}</h5>
           </div>
         </Link>
       </li>
